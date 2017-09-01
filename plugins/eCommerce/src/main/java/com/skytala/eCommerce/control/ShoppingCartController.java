5809172268fa6930e20ffde7425943fddead495f
georg <g.steinhauser1@web.de>
2017-09-01 15:19:08 +0200

    Fixed Problem with Cookies. Changed in web.xml  secure from true to false.

--- plugins/eCommerce/src/main/java/com/skytala/eCommerce/control/ShoppingCartController.java
+++ plugins/eCommerce/src/main/java/com/skytala/eCommerce/control/ShoppingCartController.java
@@ -1,30 +1,55 @@
 package com.skytala.eCommerce.control;
 
+
+import java.util.LinkedList;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lowagie.text.List;
-import com.skytala.eCommerce.entity.Product;
+import com.skytala.eCommerce.entity.Position;
+import com.skytala.eCommerce.entity.ShoppingCart;
+
+
+
+
+
 
 @RestController
 @RequestMapping("/api/cart")
 public class ShoppingCartController {
 	
 	@RequestMapping(method = RequestMethod.GET , value = "/show")
-	public List<Product> show(){
-		
+	public LinkedList<Position> show(HttpSession session){
+	
+		ShoppingCart sc = new ShoppingCart();
+		if(session.getAttribute("cart")!=null){
+			sc = (ShoppingCart) session.getAttribute("cart");
+			return sc.getPositions();
+		}
 		return null;
 	}
 	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/add", consumes = "application/x-www-form-urlencoded")
-	public boolean addToCart(){
+	public boolean addToCart(HttpSession session){
+	
+		if(session.getAttribute("cart") == null){
+			
+			ShoppingCart sc = new ShoppingCart();
+			session.setAttribute("cart",sc);
+
+			return true;
+		}
 		
 		
-		return true;
+		
+		return false;
 	}
 	@RequestMapping(method = RequestMethod.POST, value ="/remove", consumes = "application/x-www-form-urlencoded")
-	public boolean removeFromCart(){
+	public boolean removeFromCart(HttpSession session){
 		
 		
 		return true;
