bd8bdfa3f582e32dea0d96f7d9bac315236f34d9
georg <g.steinhauser1@web.de>
2017-09-04 10:04:47 +0200

    Updated Order and OrderHeader

--- plugins/eCommerce/src/main/java/com/skytala/eCommerce/entity/OrderHeader.java
+++ plugins/eCommerce/src/main/java/com/skytala/eCommerce/entity/OrderHeader.java
@@ -1,18 +1,19 @@
 package com.skytala.eCommerce.entity;
 
-import java.util.Date;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 
-public class Order {
+public class OrderHeader {
 
 	private String orderId;
 	private String orderTypeId;
 	private String orderName;
 	private String externalId;
 	private String salesChannelEnumId;
-	private Date orderDate;
+	private Timestamp orderDate;
 	private Boolean priority;
-	private Date entryDate;
-	private Date pickSheetPrintedDate;
+	private Timestamp entryDate;
+	private Timestamp pickSheetPrintedDate;
 	private String visitId;
 	private String statusId;
 	private String createdBy;
@@ -29,185 +30,241 @@ public class Order {
 	private Boolean needsInventoryIssuance;
 	private Boolean isRushOrder;
 	private String internalCode;
-	private Float remainingSubTotal;
-	private Float grandTotal;
+	private BigDecimal remainingSubTotal;
+	private BigDecimal grandTotal;
 	private Boolean isViewed;
 	private Boolean invoicePerShipment;
-	
+
 	public String getOrderId() {
 		return orderId;
 	}
+
 	public void setOrderId(String orderId) {
 		this.orderId = orderId;
 	}
+
 	public String getOrderTypeId() {
 		return orderTypeId;
 	}
+
 	public void setOrderTypeId(String orderTypeId) {
 		this.orderTypeId = orderTypeId;
 	}
+
 	public String getOrderName() {
 		return orderName;
 	}
+
 	public void setOrderName(String orderName) {
 		this.orderName = orderName;
 	}
+
 	public String getExternalId() {
 		return externalId;
 	}
+
 	public void setExternalId(String externalId) {
 		this.externalId = externalId;
 	}
+
 	public String getSalesChannelEnumId() {
 		return salesChannelEnumId;
 	}
+
 	public void setSalesChannelEnumId(String salesChannelEnumId) {
 		this.salesChannelEnumId = salesChannelEnumId;
 	}
-	public Date getOrderDate() {
+
+	public Timestamp getOrderDate() {
 		return orderDate;
 	}
-	public void setOrderDate(Date orderDate) {
+
+	public void setOrderDate(Timestamp orderDate) {
 		this.orderDate = orderDate;
 	}
+
 	public Boolean getPriority() {
 		return priority;
 	}
+
 	public void setPriority(Boolean priority) {
 		this.priority = priority;
 	}
-	public Date getEntryDate() {
+
+	public Timestamp getEntryDate() {
 		return entryDate;
 	}
-	public void setEntryDate(Date entryDate) {
+
+	public void setEntryDate(Timestamp entryDate) {
 		this.entryDate = entryDate;
 	}
-	public Date getPickSheetPrintedDate() {
+
+	public Timestamp getPickSheetPrintedDate() {
 		return pickSheetPrintedDate;
 	}
-	public void setPickSheetPrintedDate(Date pickSheetPrintedDate) {
+
+	public void setPickSheetPrintedDate(Timestamp pickSheetPrintedDate) {
 		this.pickSheetPrintedDate = pickSheetPrintedDate;
 	}
+
 	public String getVisitId() {
 		return visitId;
 	}
+
 	public void setVisitId(String visitId) {
 		this.visitId = visitId;
 	}
+
 	public String getStatusId() {
 		return statusId;
 	}
+
 	public void setStatusId(String statusId) {
 		this.statusId = statusId;
 	}
+
 	public String getCreatedBy() {
 		return createdBy;
 	}
+
 	public void setCreatedBy(String createdBy) {
 		this.createdBy = createdBy;
 	}
+
 	public String getFirstAttemptOrderId() {
 		return firstAttemptOrderId;
 	}
+
 	public void setFirstAttemptOrderId(String firstAttemptOrderId) {
 		this.firstAttemptOrderId = firstAttemptOrderId;
 	}
+
 	public String getCurrencyUom() {
 		return currencyUom;
 	}
+
 	public void setCurrencyUom(String currencyUom) {
 		this.currencyUom = currencyUom;
 	}
+
 	public String getSyncStatusId() {
 		return syncStatusId;
 	}
+
 	public void setSyncStatusId(String syncStatusId) {
 		this.syncStatusId = syncStatusId;
 	}
+
 	public String getBillingAccountId() {
 		return billingAccountId;
 	}
+
 	public void setBillingAccountId(String billingAccountId) {
 		this.billingAccountId = billingAccountId;
 	}
+
 	public String getOriginFacilityId() {
 		return originFacilityId;
 	}
+
 	public void setOriginFacilityId(String originFacilityId) {
 		this.originFacilityId = originFacilityId;
 	}
+
 	public String getWebSiteId() {
 		return webSiteId;
 	}
+
 	public void setWebSiteId(String webSiteId) {
 		this.webSiteId = webSiteId;
 	}
+
 	public String getProductStoreId() {
 		return productStoreId;
 	}
+
 	public void setProductStoreId(String productStoreId) {
 		this.productStoreId = productStoreId;
 	}
+
 	public String getTerminalId() {
 		return terminalId;
 	}
+
 	public void setTerminalId(String terminalId) {
 		this.terminalId = terminalId;
 	}
+
 	public String getTransactionId() {
 		return transactionId;
 	}
+
 	public void setTransactionId(String transactionId) {
 		this.transactionId = transactionId;
 	}
+
 	public String getAutoOrderShoppingListId() {
 		return autoOrderShoppingListId;
 	}
+
 	public void setAutoOrderShoppingListId(String autoOrderShoppingListId) {
 		this.autoOrderShoppingListId = autoOrderShoppingListId;
 	}
+
 	public Boolean getNeedsInventoryIssuance() {
 		return needsInventoryIssuance;
 	}
+
 	public void setNeedsInventoryIssuance(Boolean needsInventoryIssuance) {
 		this.needsInventoryIssuance = needsInventoryIssuance;
 	}
+
 	public Boolean getIsRushOrder() {
 		return isRushOrder;
 	}
+
 	public void setIsRushOrder(Boolean isRushOrder) {
 		this.isRushOrder = isRushOrder;
 	}
+
 	public String getInternalCode() {
 		return internalCode;
 	}
+
 	public void setInternalCode(String internalCode) {
 		this.internalCode = internalCode;
 	}
-	public Float getRemainingSubTotal() {
+
+	public BigDecimal getRemainingSubTotal() {
 		return remainingSubTotal;
 	}
-	public void setRemainingSubTotal(Float remainingSubTotal) {
+
+	public void setRemainingSubTotal(BigDecimal remainingSubTotal) {
 		this.remainingSubTotal = remainingSubTotal;
 	}
-	public Float getGrandTotal() {
+
+	public BigDecimal getGrandTotal() {
 		return grandTotal;
 	}
-	public void setGrandTotal(Float grandTotal) {
+
+	public void setGrandTotal(BigDecimal grandTotal) {
 		this.grandTotal = grandTotal;
 	}
+
 	public Boolean getIsViewed() {
 		return isViewed;
 	}
+
 	public void setIsViewed(Boolean isViewed) {
 		this.isViewed = isViewed;
 	}
+
 	public Boolean getInvoicePerShipment() {
 		return invoicePerShipment;
 	}
+
 	public void setInvoicePerShipment(Boolean invoicePerShipment) {
 		this.invoicePerShipment = invoicePerShipment;
 	}
-	
-	
-	}
+
+}
