81b643367bca4b85111767aecf871150ce7f90f5
georg <g.steinhauser1@web.de>
2017-09-04 16:36:32 +0200

    Generated Party/Person/UserLogin + Mapper

---
+++ plugins/eCommerce/src/main/java/com/skytala/eCommerce/entity/Party.java
@@ -0,0 +1,116 @@
+package com.skytala.eCommerce.entity;
+
+import java.sql.Timestamp;
+
+public class Party {
+
+	private String partyId;
+	private String partyTypeId;
+	private String externalId;
+	private String preferredCurrencyUomId;
+	private String description;
+	private String statusId;
+	private Timestamp createdDate;
+	private String createdByUserLogin;
+	private Timestamp lastModifiedDate;
+	private String lastModifiedByUserLogin;
+	private String dataSourceId;
+	private Boolean isUnread;
+
+	public String getPartyId() {
+		return partyId;
+	}
+
+	public void setPartyId(String partyId) {
+		this.partyId = partyId;
+	}
+
+	public String getPartyTypeId() {
+		return partyTypeId;
+	}
+
+	public void setPartyTypeId(String partyTypeId) {
+		this.partyTypeId = partyTypeId;
+	}
+
+	public String getExternalId() {
+		return externalId;
+	}
+
+	public void setExternalId(String externalId) {
+		this.externalId = externalId;
+	}
+
+	public String getPreferredCurrencyUomId() {
+		return preferredCurrencyUomId;
+	}
+
+	public void setPreferredCurrencyUomId(String preferredCurrencyUomId) {
+		this.preferredCurrencyUomId = preferredCurrencyUomId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatusId() {
+		return statusId;
+	}
+
+	public void setStatusId(String statusId) {
+		this.statusId = statusId;
+	}
+
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getCreatedByUserLogin() {
+		return createdByUserLogin;
+	}
+
+	public void setCreatedByUserLogin(String createdByUserLogin) {
+		this.createdByUserLogin = createdByUserLogin;
+	}
+
+	public Timestamp getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Timestamp lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getLastModifiedByUserLogin() {
+		return lastModifiedByUserLogin;
+	}
+
+	public void setLastModifiedByUserLogin(String lastModifiedByUserLogin) {
+		this.lastModifiedByUserLogin = lastModifiedByUserLogin;
+	}
+
+	public String getDataSourceId() {
+		return dataSourceId;
+	}
+
+	public void setDataSourceId(String dataSourceId) {
+		this.dataSourceId = dataSourceId;
+	}
+
+	public Boolean getIsUnread() {
+		return isUnread;
+	}
+
+	public void setIsUnread(Boolean isUnread) {
+		this.isUnread = isUnread;
+	}
+
+}
