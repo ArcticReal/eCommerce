1a03710fc622137c9e01cb6cca38d69a003a7233
georg <g.steinhauser1@web.de>
2017-09-05 15:24:51 +0200

    Not Finished but focus now on Generating thes Classes.

--- plugins/eCommerce/src/main/java/com/skytala/eCommerce/control/PersonController.java
+++ plugins/eCommerce/src/main/java/com/skytala/eCommerce/control/PersonController.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.skytala.eCommerce.command.AddPerson;
 import com.skytala.eCommerce.command.AddUserLogin;
+import com.skytala.eCommerce.command.UpdateLogin;
 import com.skytala.eCommerce.entity.Person;
 import com.skytala.eCommerce.entity.PersonMapper;
 import com.skytala.eCommerce.entity.Product;
 import com.skytala.eCommerce.entity.UserLogin;
 import com.skytala.eCommerce.entity.UserLoginMapper;
+import com.skytala.eCommerce.event.LoginUpdated;
 import com.skytala.eCommerce.event.PersonAdded;
+import com.skytala.eCommerce.event.ProductUpdated;
 import com.skytala.eCommerce.event.UserLoginAdded;
 
 @RestController
@@ -36,6 +37,7 @@ public class PersonController {
 		try{
 			person.setPartyId("IstEgal");
 			person = PersonMapper.mapstrstr(allRequestParams);
+			
 				
 		}catch (Exception e){
 			e.printStackTrace();
@@ -105,7 +107,26 @@ public class PersonController {
 		return true;
 	}
 	
-	
+	@RequestMapping(method = RequestMethod.POST, value = "/login/update", consumes ="application/x-www-form-urlencoded")
+	public boolean updateLogin(@RequestParam Map<String,String> allRequestParams){
+		
+		String id = allRequestParams.get("userLoginId");
+		
+		UpdateLogin update = new UpdateLogin(id);
+		
+		int usedTicketId;
+
+		synchronized (ProductController.class) {
+
+			usedTicketId = requestTicketId;
+			requestTicketId++;
+		}
+		Broker.instance().subscribe(LoginUpdated.class, event -> sendUserLoginChangedMessage(((ProductUpdated) event).isSuccess(), usedTicketId));
+		
+		
+		
+		return true;
+	}
 	
 	public void sendPersonChangedMessage(boolean success, int id) {
 		commandReturnVal.put(id, success);
---
+++ plugins/eCommerce/src/main/java/com/skytala/eCommerce/event/LoginUpdated.java
@@ -0,0 +1,20 @@
+package com.skytala.eCommerce.event;
+
+import com.skytala.eCommerce.control.Event;
+
+public class LoginUpdated implements Event {
+
+	private boolean success;
+
+	public LoginUpdated(boolean success) {
+		this.setSuccess(success);
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+}
