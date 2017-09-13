44f931c3485b807dddde6e60cc21fee04785312e
Dennis <dennis.horn@stud.hs-kempten.de>
2017-09-13 15:14:43 +0200

    added contact data to config

--- plugins/eCommerce/src/main/java/com/skytala/eCommerce/config/SwaggerConfig.java
+++ plugins/eCommerce/src/main/java/com/skytala/eCommerce/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,12 +30,13 @@ public class SwaggerConfig{
 
 	private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("TITLE")
-            .description("DESCRIPTION")
+            .title("Swagger")
+            .description("Swagger 2 api")
             .version("VERSION")
             .termsOfServiceUrl("http://terms-of-services.url")
             .license("LICENSE")
             .licenseUrl("http://url-to-license.com")
+            .contact(new Contact("CONTACT_NAME", "CONTACT_URL", "CONTACT_MAIL"))
             .build();
     }
 	
