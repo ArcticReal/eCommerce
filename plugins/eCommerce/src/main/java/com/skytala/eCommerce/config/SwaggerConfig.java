184575ca8e6e06b3e3ff26de5842b6222204884d
Dennis <dennis.horn@stud.hs-kempten.de>
2017-09-28 16:53:19 +0200

    SwaggerConfig disables swagger now if it is set in the conf (i dont need swagger that much while working with the backend)

--- plugins/eCommerce/src/main/java/com/skytala/eCommerce/config/SwaggerConfig.java
+++ plugins/eCommerce/src/main/java/com/skytala/eCommerce/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package com.skytala.eCommerce.config;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,17 +20,43 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig{
 	
+	private boolean enableSwagger;
+	
 	@Bean
 	public Docket api() {
 
 		
-		boolean enableSwagger = true;
         if(System.getProperty("sun.java.command").contains("--test")) {
         	enableSwagger = false;
+        }else {
+        	
+        	String configLocation = System.getProperty("user.dir")+"/plugins/eCommerce/config/swagger.conf";
+        	File f = new File(configLocation);
+        	
+        	try {
+        		Scanner s = new Scanner(f);
+        		
+        		if(s.hasNextLine()) {
+        			String line = s.nextLine();
+        			
+        			if(line.split("=")[0].trim().contains("enabled")) {
+        				if(line.split("=")[1].trim().contains("true")) {
+        					enableSwagger = true;
+        				}else {
+        					enableSwagger = false;
+        				}
+        			}
+        		}
+        		
+        		s.close();
+        	} catch (FileNotFoundException e) {
+        		
+        		System.err.println("File "+configLocation+" not found. Enabling Swagger by default");
+				enableSwagger = true;
+        	}
+        	
         }
  
-        
-        
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.any())
