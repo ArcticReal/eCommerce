00c06f4fd09165bf70c2848fbb2731e46bfc8be7
Dennis <dennis.horn@stud.hs-kempten.de>
2017-10-20 09:23:17 +0200

    service controller are now annotated with crossorigin

--- plugins/eCommerce/src/main/java/com/skytala/eCommerce/service/accounting/AccountingCybersource.java
+++ plugins/eCommerce/src/main/java/com/skytala/eCommerce/service/accounting/AccountingCybersource.java
@@ -15,12 +15,14 @@ import org.apache.ofbiz.service.ServiceAuthException;
 import org.apache.ofbiz.service.ServiceValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/service/AccountingCybersource")
 public class AccountingCybersource{
