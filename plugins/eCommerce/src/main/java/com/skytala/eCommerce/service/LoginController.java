5987c67bac734a46c8541fb8d42df2754c156935
Dennis <dennis.horn@stud.hs-kempten.de>
2017-10-30 10:53:15 +0100

    logout function; DTO for loggedInPersonName

--- plugins/eCommerce/src/main/java/com/skytala/eCommerce/service/LoginController.java
+++ plugins/eCommerce/src/main/java/com/skytala/eCommerce/service/LoginController.java
@@ -1,5 +1,8 @@
 package com.skytala.eCommerce.service;
 
+import com.skytala.eCommerce.domain.party.relations.person.dto.LoggedInPersonNameDTO;
+import com.skytala.eCommerce.domain.party.relations.person.mapper.PersonMapper;
+import org.apache.ofbiz.entity.GenericValue;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static com.skytala.eCommerce.framework.pubsub.ResponseUtil.badRequest;
+import static com.skytala.eCommerce.framework.pubsub.ResponseUtil.successful;
+import static com.skytala.eCommerce.framework.pubsub.ResponseUtil.unauthorized;
+
 
 @RestController
 public class LoginController {
@@ -14,12 +23,38 @@ public class LoginController {
     @RequestMapping("/login")
     public void doLoginForwarded(HttpServletRequest request, HttpServletResponse response) throws Exception{
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");//TODO look for origins to allow access from
+        response.setHeader("Access-Control-Allow-Origin", "http://192.168.49.60:3000");//TODO look for origins to allow access from
         response.setHeader("Access-Control-Allow-Credentials", "true");
         RequestDispatcher rd = request.getRequestDispatcher("/control/login");
         rd.forward(request, response);
     }
 
+    @RequestMapping("/logout")
+    public void doLogoutForwarded(HttpServletRequest request, HttpServletResponse response) throws Exception{
 
+        response.setHeader("Access-Control-Allow-Origin", "http://192.168.49.60:3000");//TODO look for origins to allow access from
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        RequestDispatcher rd = request.getRequestDispatcher("/control/logout");
+        rd.forward(request, response);
+    }
+
+    @RequestMapping("/loggedInPerson")
+    public ResponseEntity getUserLoginName(HttpSession session){
+
+        return successful(session.getAttribute("person"));
+
+    }
+
+    @RequestMapping("/loggedInPersonName")
+    public ResponseEntity<LoggedInPersonNameDTO> getLoggedInPersonName(HttpSession session){
+        GenericValue person = (GenericValue)session.getAttribute("person");
+
+        if(person != null){
+            LoggedInPersonNameDTO dto = new LoggedInPersonNameDTO(PersonMapper.map(person));
+            return successful(dto);
+        }
+
+        return unauthorized();
+    }
 
 }
